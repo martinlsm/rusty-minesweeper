350aaaa155fbf08959da881abdf47a52d91da998
Martin Lindström <martin.lindstrom94@gmail.com>
2022-02-05 10:14:13 +0100

    Extend program towards a minimal working CLI
    Still not working yet.

--- src/board.rs
+++ src/board.rs
@@ -1,16 +1,18 @@
 use itertools::Itertools;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
-use std::cmp::{min, max};
-use std::collections::{HashMap, HashSet};
+use std::cmp::{max, min};
+use std::collections::{HashSet, VecDeque};
+use std::iter::Iterator;
 
 pub trait Board {
     fn reveal(&mut self, r: usize, c: usize);
     fn get_width(&self) -> usize;
     fn get_height(&self) -> usize;
+    fn debug_print(&self);
 }
 
-#[derive(Clone)]
+#[derive(Clone, PartialEq)]
 pub enum TileState {
     // true if flagged, else false.
     HIDDEN(bool),
@@ -28,38 +30,26 @@ struct BoardImpl {
 impl Board for BoardImpl {
     fn reveal(&mut self, r: usize, c: usize) {
         if self.mines.is_empty() {
-            // Unitialized board; place mines at random positions.
-            let mut rng = thread_rng();
-            let mut mine_free_tiles: Vec<(usize, usize)> = (0..self.get_width())
-                .cartesian_product((0..self.get_height()))
-                .collect();
-            mine_free_tiles.shuffle(&mut rng);
-            for p in mine_free_tiles.iter().take(self.mines_count) {
-                self.mines.insert(*p);
-            }
+            self.setup_mines((r, c));
         }
 
-        if self.mines.contains(&(r,c)) {
+        if self.mines.contains(&(r, c)) {
             self.tiles[r][c] = TileState::REVEALED(-1);
             return;
         }
 
         let mut neighbor_mines_count = 0;
-        for rr in (max(r-1,0)..min(r+2,self.get_height())) {
-            for cc in (max(c-1,0)..min(c+2,self.get_width())) {
+        for (rr, cc) in self.get_neighbors((r, c)) {
                 if rr == r && cc == c {
                     continue;
                 }
-                
-                if let TileState::REVEALED(x) = self.tiles[rr][cc] {
-                    if x == -1 {
-                        neighbor_mines_count += 1;
-                    }
+
+                if self.mines.contains(&(rr, cc)) {
+                    neighbor_mines_count += 1;
                 }
-            }
         }
-        
-        self.tiles[r][c] = TileState::REVEALED(neighbor_mines_count);        
+
+        self.tiles[r][c] = TileState::REVEALED(neighbor_mines_count);
     }
 
     fn get_width(&self) -> usize {
@@ -69,11 +59,81 @@ impl Board for BoardImpl {
     fn get_height(&self) -> usize {
         self.tiles[0].len()
     }
+
+    fn debug_print(&self) {
+        let mut s = String::new();
+        for r in 0..self.get_height() {
+            for c in 0..self.get_width() {
+                match self.tiles[r][c] {
+                    TileState::HIDDEN(_) => s.push_str("█"),
+                    TileState::REVEALED(x) => s.push_str(&x.to_string()[..1]),
+                }
+            }
+            s.push_str("\n");
+        }
+        println!("{}", s);
+    }
+}
+
+impl BoardImpl {
+    fn setup_mines(&mut self, exclude: (usize, usize)) {
+        // Unitialized board; place mines at random positions.
+        let mut rng = thread_rng();
+        let mut mine_free_tiles: Vec<(usize, usize)> = (0..self.get_width())
+            .cartesian_product(0..self.get_height())
+            .filter(|&p| p != exclude)
+            .collect();
+        mine_free_tiles.shuffle(&mut rng);
+        for p in mine_free_tiles.iter().take(self.mines_count) {
+            self.mines.insert(*p);
+        }
+    }
+
+    fn expand_revealed_tile(&mut self, tile: (usize, usize)) {
+        // Use DFS.
+        let mut q: VecDeque<(usize, usize)> = VecDeque::new();
+        let mut s: HashSet<(usize, usize)> = HashSet::new();
+        q.push_back(tile);
+
+        while !q.is_empty() {
+            let (r, c) = q.pop_back().unwrap();
+
+            match self.tiles[r][c] {
+                TileState::REVEALED(0) => (),
+                TileState::REVEALED(_) => continue,
+                TileState::HIDDEN(_) => {
+                    if !self.mines.contains(&(r, c)) {
+                        self.reveal(r, c);
+                    }
+                },
+            }
+
+            // if self.tiles[r][c] != TileState::REVEALED(0)
+            //     continue;
+            // }
+
+            for neighbor in self.get_neighbors((r, c)) {
+                if !s.contains(&neighbor) {
+                    q.push_back(neighbor);
+                    s.insert(neighbor);
+                }
+            }
+        }
+    }
+
+    fn get_neighbors(&self, tile: (usize, usize)) -> Vec<(usize, usize)> {
+        let from_r = max((tile.0 as i64) - 1, 0) as usize;
+        let to_r = min(tile.0 + 2, self.get_height());
+        let from_c = max((tile.1 as i64) - 1, 0) as usize;
+        let to_c = min(tile.1 + 2, self.get_width());
+
+        (from_r..to_r).cartesian_product(from_c..to_c).filter(|&p| p != tile).collect()
+    }
 }
 
 pub fn new(width: usize, height: usize) -> Box<dyn Board> {
     let mut tiles = Vec::new();
-    for r in 0..height {
+    for _ in 0..height {
         tiles.push(vec![TileState::HIDDEN(false); width]);
     }
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,24 @@
+use std::io;
+
 mod board;
 
 fn main() {
-    let board = board::new(9, 9);
+    let mut board = board::new(9, 9);
+    loop {
+        let mut user_input = String::new();
+        if let Err(_) = io::stdin().read_line(&mut user_input) {
+            println!("Could not read user input");
+            break;
+        }
+
+        let tile: Vec<usize> = user_input
+            .trim()
+            .split(" ")
+            .map(|x| x.parse::<usize>().unwrap())
+            .take(2)
+            .collect();
+
+        board.reveal(tile[0], tile[1]);
+        board.debug_print();
+    }
 }
